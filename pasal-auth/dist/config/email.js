57701f9effb7e2339d458d8f9488f3a1b8594bf5
Bharat Shah <akashche72@gmail.com>
2023-08-11 16:35:52 +0545

    dist file is deleted

--- pasal-auth/dist/config/email.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mailerEmail = void 0;
-exports.mailerEmail = "support@customize.io";
-//# sourceMappingURL=email.js.map
