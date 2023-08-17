c70b35b19a0e4c90dde87299c6e1647e4682e682
Bharat Shah <akashche72@gmail.com>
2023-08-17 16:02:04 +0545

    starting first deployment

--- pasal-orders/src/test/setup.ts
+++ pasal-orders/src/test/setup.ts
@@ -19,8 +19,8 @@ beforeAll(async () => {
   process.env.NODE_ENV = "test";
   process.env.JWT_KEY = "asdf";
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-  mongo = new MongoMemoryServer();
-  const mongoUri = await mongo.getUri();
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
 
   await mongoose.connect(mongoUri, {
     useNewUrlParser: true,
