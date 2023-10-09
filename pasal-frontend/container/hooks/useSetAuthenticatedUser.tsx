bcb0fe50ace5b35cc8f4fca91be58112f3340c28
Bharat Shah <akashche72@gmail.com>
2023-10-09 18:43:27 +0545

    while checking if user is authenticated no showing he ui to the user which is protected and showing loader and then either redirect or show the auth ui

--- pasal-frontend/container/hooks/useSetAuthenticatedUser.tsx
+++ pasal-frontend/container/hooks/useSetAuthenticatedUser.tsx
@@ -6,30 +6,36 @@ import { RootState } from '../src/store';
 import { APIS } from '../src/apis';
 
 interface AuthenticatedUserInterface {
-    setLoading?: Function;
+    setLoading: Function;
 }
-export default function useSetAuthenticatedUser() {
-    
-    const { auth:{auth} } = useSelector((state: RootState) => state);
+export default function useSetAuthenticatedUser({ setLoading }: AuthenticatedUserInterface) {
+
+    const { auth: { auth } } = useSelector((state: RootState) => state);
     const dispatch = useDispatch();
     useEffect(() => {
         const fetchCurrentUser = async () => {
+
             try {
                 const { currentUser } = await request({
                     url: APIS.auth.currentUser,
                     method: 'get'
                 });
                 dispatch(authenticatedUser(currentUser));
-                
+                setLoading(false);
+
             } catch (err) {
                 console.error('Count not fetch current user', err);
             }
-            
-        }
-        
 
+        }
+        if (auth) {
+            setLoading(false);
+        }
         if (!auth) {
+            setLoading(true);
             fetchCurrentUser();
+
+
         }
     }, []);
     return null;
--- pasal-frontend/container/src/components/common/Container.tsx
+++ pasal-frontend/container/src/components/common/Container.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import ArrowRight from "../../assets/svg/arrow-right.svg";
 import { menuIds } from "../../config/navMenu";
 import SideMenu from "./SideMenu";
+// import './loader.scss';
 
 import useSetAuthenticatedUser from "../../../hooks/useSetAuthenticatedUser";
 interface ContainerInterface {
@@ -18,41 +19,50 @@ interface ContainerInterface {
 // globalDispatch={dispatch}
 export default function Container({ setShowSettingModel, showSettingModel, children, setSelectedMenu, selectedMenu, setShowProfileSideModel, actions, globalDispatch }: ContainerInterface) {
     // If user is not logged in then simply redirect user immediately out to 
-    // const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(true);
 
 
-    useSetAuthenticatedUser();
+    useSetAuthenticatedUser({setLoading});
 
-    return (
-        <>
-            {/* {loading && <div>Please wait</div>} */}
+    if(loading) {
+        return <div className="loading">
+            <div className="loader">Please wait....</div>
+        </div>
+    } 
 
-            <input type="radio" id="toggle-menu-checkbox" className="toggle-menu-checbox" name="toggle-menu-checkbox" />
-            <label htmlFor="toggle-menu-checkbox" >
-                <div className="toggal-menu">
-                    <ArrowRight />
+    if(!loading) {
+        return (
+            <>
+                <input type="radio" id="toggle-menu-checkbox" className="toggle-menu-checbox" name="toggle-menu-checkbox" />
+                <label htmlFor="toggle-menu-checkbox" >
+                    <div className="toggal-menu">
+                        <ArrowRight />
+                    </div>
+                </label>
+                { }
+                <div className="container">
+                    <div className="left side--navbar hide">
+                        <SideMenu
+                            setSelectedMenu={setSelectedMenu}
+                            setShowProfileSideModel={setShowProfileSideModel}
+                            setShowSettingModel={setShowSettingModel}
+                            showSettingModel={showSettingModel}
+                            actions={actions}
+                            globalDispatch={globalDispatch}
+                        />
+                    </div>
+                    <div className="right services">
+    
+                        {children}
+                    </div>
                 </div>
-            </label>
-            { }
-            <div className="container">
-                <div className="left side--navbar hide">
-                    <SideMenu
-                        setSelectedMenu={setSelectedMenu}
-                        setShowProfileSideModel={setShowProfileSideModel}
-                        setShowSettingModel={setShowSettingModel}
-                        showSettingModel={showSettingModel}
-                        actions={actions}
-                        globalDispatch={globalDispatch}
-                    />
-                </div>
-                <div className="right services">
+    
+            </>
+    
+    
+        )
+    }
 
-                    {children}
-                </div>
-            </div>
-
-        </>
-
-
-    )
+    return null;
+    
 }
