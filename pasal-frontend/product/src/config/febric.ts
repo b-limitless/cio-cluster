88fb329f8589e3997bcd867b35aa83e4674bf8e1
Bharat Shah <akashche72@gmail.com>
2023-10-16 18:55:27 +0545

    type and stretcy options is added

--- pasal-frontend/product/src/config/febric.ts
+++ pasal-frontend/product/src/config/febric.ts
@@ -1,37 +1,37 @@
 export const febricTypes = [
-  { name: "Cotton", code: "cotton" },
-  { name: "Denim", code: "denim" },
-  { name: "Fleece", code: "fleece" },
-  { name: "Linen", code: "linen" },
-  { name: "Lycra/Spandex", code: "lycra_spandex" },
-  { name: "Nylon", code: "nylon" },
-  { name: "Polyester", code: "polyester" },
-  { name: "Rayon", code: "rayon" },
-  { name: "Satin", code: "satin" },
-  { name: "Silk", code: "silk" },
-  { name: "Velvet", code: "velvet" },
-  { name: "Wool", code: "wool" },
+  { name: 'Cotton', code: 'cotton' },
+  { name: 'Denim', code: 'denim' },
+  { name: 'Fleece', code: 'fleece' },
+  { name: 'Linen', code: 'linen' },
+  { name: 'Lycra/Spandex', code: 'lycra_spandex' },
+  { name: 'Nylon', code: 'nylon' },
+  { name: 'Polyester', code: 'polyester' },
+  { name: 'Rayon', code: 'rayon' },
+  { name: 'Satin', code: 'satin' },
+  { name: 'Silk', code: 'silk' },
+  { name: 'Velvet', code: 'velvet' },
+  { name: 'Wool', code: 'wool' },
 ];
 
 export const febricSeasons = [
-  { name: "Winter", code: "winter" },
-  { name: "Spring", code: "spring" },
-  { name: "Summer", code: "summer" },
-  { name: "Fall", code: "fall" },
-  { name: "Winter", code: "winter" },
+  { name: 'Winter', code: 'winter' },
+  { name: 'Spring', code: 'spring' },
+  { name: 'Summer', code: 'summer' },
+  { name: 'Fall', code: 'fall' },
+  { name: 'Winter', code: 'winter' },
 ];
 
 export const threadTypes = [
-  { name: "Cotton", code: "cotton" },
-  { name: "Polyester", code: "polyester" },
-  { name: "Silk", code: "silk" },
-  { name: "Nylon", code: "nylon" },
-  { name: "Metallic", code: "metallic" },
-  { name: "Embroidery", code: "embroidery" },
-  { name: "Quilting", code: "quilting" },
-  { name: "Invisible", code: "invisible" },
-  { name: "Upholstery", code: "upholstery" },
-  { name: "Overlock", code: "overlock" },
+  { name: 'Cotton', code: 'cotton' },
+  { name: 'Polyester', code: 'polyester' },
+  { name: 'Silk', code: 'silk' },
+  { name: 'Nylon', code: 'nylon' },
+  { name: 'Metallic', code: 'metallic' },
+  { name: 'Embroidery', code: 'embroidery' },
+  { name: 'Quilting', code: 'quilting' },
+  { name: 'Invisible', code: 'invisible' },
+  { name: 'Upholstery', code: 'upholstery' },
+  { name: 'Overlock', code: 'overlock' },
 ];
 
 /*
@@ -50,139 +50,139 @@ It is measured in candelas per square meter (cd/m²) or nits.
 */
 
 export const brightness = [
-  { name: "Reflectance", code: "reflectance" },
-  { name: "Luminance", code: "luminance" },
+  { name: 'Reflectance', code: 'reflectance' },
+  { name: 'Luminance', code: 'luminance' },
 ];
 
 export const superShiny = [
-  { name: "", code: "" },
-  { name: "Yes", code: true },
-  { name: "No", code: false },
+  { name: '', code: '' },
+  { name: 'Yes', code: true },
+  { name: 'No', code: false },
 ];
 
 export const threadCounts = [
-  { code: "200-400", name: "Standard Quality" },
-  { code: "400-600", name: "Luxury Grade" },
-  { code: "600+", name: "Premium Range" },
+  { code: '200-400', name: 'Standard Quality' },
+  { code: '400-600', name: 'Luxury Grade' },
+  { code: '600+', name: 'Premium Range' },
 ];
 
 export const opacity = [
-  { name: "Transparent (0%)", code: 0 },
-  { name: "Semi-Transparent (25%)", code: 25 },
-  { name: "Semi-Opaque (50%)", code: 50 },
-  { name: "Opaque (75%)", code: 75 },
-  { name: "Fully Opaque (100%)", code: 100 },
+  { name: 'Transparent (0%)', code: 0 },
+  { name: 'Semi-Transparent (25%)', code: 25 },
+  { name: 'Semi-Opaque (50%)', code: 50 },
+  { name: 'Opaque (75%)', code: 75 },
+  { name: 'Fully Opaque (100%)', code: 100 },
 ];
 
 export const waterProof = [
-  { name: "Not Waterproof", code: "not_waterproof" },
-  { name: "Water-Resistant", code: "water_resistant" },
-  { name: "Waterproof", code: "waterproof" },
+  { name: 'Not Waterproof', code: 'not_waterproof' },
+  { name: 'Water-Resistant', code: 'water_resistant' },
+  { name: 'Waterproof', code: 'waterproof' },
 ];
 
 export const characters = [
   {
-    name: "New",
-    code: "New",
-    description: "Indicates newly manufactured fabrics.",
+    name: 'New',
+    code: 'New',
+    description: 'Indicates newly manufactured fabrics.',
   },
   {
-    name: "Eco-Friendly/Sustainable",
-    code: "Eco-Friendly/Sustainable",
+    name: 'Eco-Friendly/Sustainable',
+    code: 'Eco-Friendly/Sustainable',
     description:
-      "Denotes fabrics made from environmentally friendly materials or produced using sustainable manufacturing practices.",
+      'Denotes fabrics made from environmentally friendly materials or produced using sustainable manufacturing practices.',
   },
   {
-    name: "Easy Iron",
-    code: "Easy_Iron",
+    name: 'Easy Iron',
+    code: 'Easy_Iron',
     description:
-      "Refers to fabrics that are designed to be easily ironed and require minimal effort to remove wrinkles.",
+      'Refers to fabrics that are designed to be easily ironed and require minimal effort to remove wrinkles.',
   },
   {
-    name: "Breathable",
-    code: "Breathable",
+    name: 'Breathable',
+    code: 'Breathable',
     description:
-      "Indicates fabrics that allow air circulation, promoting comfort and reducing moisture buildup.",
+      'Indicates fabrics that allow air circulation, promoting comfort and reducing moisture buildup.',
   },
   {
-    name: "Stretchy/Elastic",
-    code: "Stretchy_Elastic",
+    name: 'Stretchy/Elastic',
+    code: 'Stretchy_Elastic',
     description:
-      "Refers to fabrics with stretch properties, allowing them to stretch and recover without losing their shape.",
+      'Refers to fabrics with stretch properties, allowing them to stretch and recover without losing their shape.',
   },
   {
-    name: "Wrinkle-Resistant",
-    code: "Wrinkle-Resistant",
+    name: 'Wrinkle-Resistant',
+    code: 'Wrinkle-Resistant',
     description:
-      "Indicates that the fabric is less prone to wrinkling, requiring minimal ironing or maintaining a smoother appearance.",
+      'Indicates that the fabric is less prone to wrinkling, requiring minimal ironing or maintaining a smoother appearance.',
   },
   {
-    name: "Lightweight",
-    code: "Lightweight",
+    name: 'Lightweight',
+    code: 'Lightweight',
     description:
-      "Describes fabrics that are light in weight, often preferred for warm weather or when a lighter feel is desired.",
+      'Describes fabrics that are light in weight, often preferred for warm weather or when a lighter feel is desired.',
   },
   {
-    name: "UV-Resistant",
-    code: "UV-Resistant",
+    name: 'UV-Resistant',
+    code: 'UV-Resistant',
     description:
-      "Refers to fabrics that offer protection against harmful UV rays, reducing the risk of sunburn and skin damage.",
+      'Refers to fabrics that offer protection against harmful UV rays, reducing the risk of sunburn and skin damage.',
   },
   {
-    name: "Quick-Drying",
-    code: "Quick-Drying",
+    name: 'Quick-Drying',
+    code: 'Quick-Drying',
     description:
-      "Indicates that the fabric has moisture-wicking properties and dries rapidly, making it suitable for activewear or situations where fast drying is desired.",
+      'Indicates that the fabric has moisture-wicking properties and dries rapidly, making it suitable for activewear or situations where fast drying is desired.',
   },
   {
-    name: "Anti-Microbial",
-    code: "Anti-Microbial",
+    name: 'Anti-Microbial',
+    code: 'Anti-Microbial',
     description:
-      "Describes fabrics that inhibit the growth of microorganisms, reducing odors and maintaining freshness.",
+      'Describes fabrics that inhibit the growth of microorganisms, reducing odors and maintaining freshness.',
   },
   {
-    name: "Flame-Retardant",
-    code: "Flame-Retardant",
+    name: 'Flame-Retardant',
+    code: 'Flame-Retardant',
     description:
-      "Indicates that the fabric has been treated to reduce its flammability or slow down the spread of flames.",
+      'Indicates that the fabric has been treated to reduce its flammability or slow down the spread of flames.',
   },
   {
-    name: "Stain-Resistant",
-    code: "Stain-Resistant",
+    name: 'Stain-Resistant',
+    code: 'Stain-Resistant',
     description:
-      "Refers to fabrics that are resistant to staining, making it easier to clean and maintain their appearance.",
+      'Refers to fabrics that are resistant to staining, making it easier to clean and maintain their appearance.',
   },
 ];
 
 export const warmth = [
   {
-    name: "",
-    code: "",
-    description: "",
+    name: '',
+    code: '',
+    description: '',
   },
   {
-    name: "Thermal Conductivity",
-    code: "Thermal_Conductivity",
+    name: 'Thermal Conductivity',
+    code: 'Thermal_Conductivity',
     description:
-      "This measures the ability of a fabric to conduct heat. A lower thermal conductivity indicates better insulation as it restricts the transfer of heat through the fabric.",
+      'This measures the ability of a fabric to conduct heat. A lower thermal conductivity indicates better insulation as it restricts the transfer of heat through the fabric.',
   },
   {
-    name: "Thermal Resistance",
-    code: "Thermal_Resistance",
+    name: 'Thermal Resistance',
+    code: 'Thermal_Resistance',
     description:
-      "Also known as R-value, it represents the fabric's resistance to heat flow. Higher thermal resistance implies better insulation and increased warmth.",
+      'Also known as R-value, it represents the fabric's resistance to heat flow. Higher thermal resistance implies better insulation and increased warmth.',
   },
   {
-    name: "Clo Value",
-    code: "Clo_Value",
+    name: 'Clo Value',
+    code: 'Clo_Value',
     description:
-      "The clo value measures the insulation level provided by a specific fabric. One clo is equal to the amount of insulation needed to maintain comfort at rest in a typical indoor setting.",
+      'The clo value measures the insulation level provided by a specific fabric. One clo is equal to the amount of insulation needed to maintain comfort at rest in a typical indoor setting.',
   },
   {
-    name: "TOG Rating",
-    code: "TOG_Rating",
+    name: 'TOG Rating',
+    code: 'TOG_Rating',
     description:
-      "Commonly used for bedding and sleepwear, TOG rating measures the thermal resistance of the fabric. The higher the TOG rating, the warmer the fabric.",
+      'Commonly used for bedding and sleepwear, TOG rating measures the thermal resistance of the fabric. The higher the TOG rating, the warmer the fabric.',
   },
 ];
 
@@ -194,62 +194,71 @@ export const excellence = [
 
 export const threadStyles = [
   {
-    code: "plain",
-    name: "Plain",
+    code: 'plain',
+    name: 'Plain',
     description:
-      "A simple and classic thread style, suitable for various fabric types.",
+      'A simple and classic thread style, suitable for various fabric types.',
   },
   {
-    code: "twisted",
-    name: "Twisted",
+    code: 'twisted',
+    name: 'Twisted',
     description:
-      "Threads are twisted together, creating a durable and textured look.",
+      'Threads are twisted together, creating a durable and textured look.',
   },
   {
-    code: "embroidered",
-    name: "Embroidered",
+    code: 'embroidered',
+    name: 'Embroidered',
     description:
-      "Decorative threads are used to create intricate patterns and designs.",
+      'Decorative threads are used to create intricate patterns and designs.',
   },
   {
-    code: "metallic",
-    name: "Metallic",
+    code: 'metallic',
+    name: 'Metallic',
     description:
-      "Threads with metallic finish, adding a touch of shimmer to your fabric.",
+      'Threads with metallic finish, adding a touch of shimmer to your fabric.',
   },
   {
-    code: "contrastStitch",
-    name: "Contrast Stitch",
+    code: 'contrastStitch',
+    name: 'Contrast Stitch',
     description:
-      "Threads in contrasting color, enhancing the visibility of stitches.",
+      'Threads in contrasting color, enhancing the visibility of stitches.',
   },
 ];
 
 
 export const tones = [
   {
-    code: "light",
-    name: "Light",
-    description: "Soft and subtle shades, perfect for creating a delicate look.",
+    code: 'light',
+    name: 'Light',
+    description: 'Soft and subtle shades, perfect for creating a delicate look.',
   },
   {
-    code: "medium",
-    name: "Medium",
-    description: "Moderate tones, providing a balanced and versatile appearance.",
+    code: 'medium',
+    name: 'Medium',
+    description: 'Moderate tones, providing a balanced and versatile appearance.',
   },
   {
-    code: "dark",
-    name: "Dark",
-    description: "Rich and deep tones, adding a touch of elegance to the fabric.",
+    code: 'dark',
+    name: 'Dark',
+    description: 'Rich and deep tones, adding a touch of elegance to the fabric.',
   },
   {
-    code: "vibrant",
-    name: "Vibrant",
-    description: "Bright and lively tones, ideal for making a bold statement.",
+    code: 'vibrant',
+    name: 'Vibrant',
+    description: 'Bright and lively tones, ideal for making a bold statement.',
   },
   {
-    code: "neutral",
-    name: "Neutral",
-    description: "Natural and earthy tones, suitable for a classic and timeless look.",
+    code: 'neutral',
+    name: 'Neutral',
+    description: 'Natural and earthy tones, suitable for a classic and timeless look.',
   }
 ];
+
+
+export const stretchy = [
+  { code: 'non-stretchy', name: 'Non-Stretchy', description: 'Fabric does not stretch.' },
+  { code: 'stretchy', name: 'Stretchy', description: 'Fabric has moderate stretch.' },
+  { code: 'very-stretchy', name: 'Very Stretchy', description: 'Fabric has high elasticity.' }
+];
+
+export const type = [{code: 'shirt', name: 'Shirt'}, {code: 'pant', name: 'Pant'}]
